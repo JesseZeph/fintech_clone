0f2117cec3ff1dd42df156d003e925961f29a62d
Jesse Josiah <jessezephyr@gmail.com>
2025-03-22 23:58:30 +0100

    feat: signiup page UI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,30 @@
+import Colors from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { TouchableOpacity } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-import { useColorScheme } from '@/components/useColorScheme';
 
 export {
-  // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from 'expo-router';
-
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+const InitialLayout = () => {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     ...FontAwesome.font,
   });
+
+  const router = useRouter()
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
@@ -42,18 +41,30 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return <Stack>
+    <Stack.Screen name="index" options={{ headerShown: false }} />
+    <Stack.Screen name="signup" options={{
+      title: '', headerBackTitle: '', headerShadowVisible: false, headerStyle: {
+        backgroundColor: Colors.background
+      },
+      headerLeft: () => (
+        <TouchableOpacity onPress={router.back}>
+          <Ionicons name='arrow-back' size={34} color={Colors.dark} />
+        </TouchableOpacity>
+      )
+    }} />
+
+  </Stack>;
 }
 
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+const RootLayoutNav = () => {
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style='light' />
+      <InitialLayout />
+    </GestureHandlerRootView>
+  )
 }
+
+export default RootLayoutNav;
